528a30d77d471beafbee3ab2e2bece94d8088b97
Thanh Binh <binhnt1608@gmail.com>
2016-07-05 01:03:56 +0700

    bản cũ

---
+++ MvcAccountant/src/MvcAccountant/Models/CashDisbursement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcAccountant.Models
+{
+    public class CashDisbursement
+    {
+        [Key]
+        public string CheckNumber { get; set; }
+        public DateTime CashDisbursementDate { get; set; }
+        
+        public string EmployeeID { get; set; }
+        public string VendorID { get; set; }
+
+        public int CashAccountID { get; set; }
+        public CashAccount CashAccount { get; set; }
+        
+
+
+
+    }
+}
